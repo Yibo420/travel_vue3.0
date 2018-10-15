d01057919ca4477b896aa28777788841bd72da3f
yibo <420553413@qq.com>
2018-10-15 08:43:49 +0800

    finish city  css

---
+++ src/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul class="list">
+        <li class="item">asdfadsfaa</li>
+        <li class="item">a</li>
+        <li class="item">a</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "Alphabet"
+    }
+</script>
+
+<style scoped lang="stylus">
+@import "~@/assets/variables.styl";
+    .list{
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        position: absolute;
+        top: 1.58rem;
+        right: 0;
+        bottom: 0;
+        width: .4rem;
+        .item{
+            line-height :.4rem;
+            text-align:center;
+            color:$bgcolor;
+        }
+    }
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <alphabet></alphabet>
     </div>
 </template>
 
 <script>
     import CityHeader from '../components/CityHeader'
     import CitySearch from '../components/CitySearch'
+    import CityList from '../components/CityList'
+    import Alphabet from '../components/Alphabet'
     export default {
         name: "City",
         components:{
             CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            Alphabet
         }
     }
 </script>
